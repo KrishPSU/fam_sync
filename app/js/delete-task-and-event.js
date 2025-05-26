686b20f0661a18607ac92c131e742d46d890c7cc
KrishPSU <163033777+KrishPSU@users.noreply.github.com>
2025-05-26 00:46:24 -0400

    Create delete-task-and-event.js

---
+++ app/js/delete-task-and-event.js
@@ -0,0 +1,12 @@
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('delete-task-and-event-btn')) {
+    const item = e.target.closest('.task-item, .event-item');
+    // console.log(item);
+    if (item.classList.contains('event-item')) {
+      socket.emit('delete-event', item.id);
+    } else if (item.classList.contains('task-item')) {
+      socket.emit('delete-task', item.id);
+    }
+    item.remove(); // Or call a function to delete from database
+  }
+});

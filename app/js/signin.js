18ab6f0fb8df5e9e41a76437ca7ea4780c667ceb
KrishPSU <163033777+KrishPSU@users.noreply.github.com>
2025-05-26 00:12:20 -0400

    Update signin.js

--- app/js/signin.js
+++ app/js/signin.js
@@ -3,6 +3,14 @@ const submit_person_btn = document.querySelector('.btn-continue');
 const name_dropdown = document.getElementById('name');
 
 
+window.addEventListener('load', () => {
+  const person = localStorage.getItem("person");
+  if (person !== null) {
+    window.location.href = `/${person}`
+  }
+});
+
+
 submit_person_btn.addEventListener('click', () => {
   if (name_dropdown.value == "") return;
   console.log(name_dropdown.value);

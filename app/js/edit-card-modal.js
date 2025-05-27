b8bd8dd8368aac5862a533a61edbd83e39bbe46b
Krish <44930634+KrishSChavan@users.noreply.github.com>
2025-05-27 17:52:28 -0400

    Update edit-card-modal.js

--- app/js/edit-card-modal.js
+++ app/js/edit-card-modal.js
@@ -21,10 +21,12 @@ function close_edit_card_modal() {
 document.getElementById('editCardForm').addEventListener('submit', function(e) {
   e.preventDefault();
   const data = Object.fromEntries(new FormData(this));
+  let title = data.title;
+  let description = document.getElementById('edit-desc').value;
 
-  console.log(`Card edited:\n${data.title} | \n${data.description}`);
-  editCard(currentCardBeingEditedId, data.title, data.description);
-  socket.emit('edit-card', currentCardBeingEditedId, data.title, data.description);
+  console.log(`Card edited:\n${title} | \n${description}`);
+  editCard(currentCardBeingEditedId, title, description);
+  socket.emit('edit-card', currentCardBeingEditedId, title, description);
   close_edit_card_modal();
   this.reset();
 });

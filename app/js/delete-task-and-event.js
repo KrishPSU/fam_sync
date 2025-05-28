4c273003e415f613e1eedd3f4aa2fa17faad74b4
Krish <44930634+KrishSChavan@users.noreply.github.com>
2025-05-27 21:44:28 -0400

    Update delete-task-and-event.js

--- app/js/delete-task-and-event.js
+++ app/js/delete-task-and-event.js
@@ -1,11 +1,11 @@
 document.addEventListener('click', function (e) {
   if (e.target.classList.contains('delete-task-and-event-btn')) {
     const item = e.target.closest('.task-item, .event-item');
-    // console.log(item);
+    console.log(item);
     if (item.classList.contains('event-item')) {
       socket.emit('delete-event', item.id);
     } else if (item.classList.contains('task-item')) {
-      socket.emit('delete-task', item.id);
+      socket.emit('delete-task', item.querySelector('.task').id);
     }
     item.remove(); // Or call a function to delete from database
   }

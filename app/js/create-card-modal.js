700fc2355810d1a31c12832692bea1be983cd94e
Krish <44930634+KrishSChavan@users.noreply.github.com>
2025-05-28 23:26:12 -0400

    Update create-card-modal.js

--- app/js/create-card-modal.js
+++ app/js/create-card-modal.js
@@ -1,5 +1,7 @@
 const new_card_button = document.querySelector('.new-card-button');
 const close_new_card_modal_button = document.querySelector('#close-card-modal-btn');
+const card_form = document.getElementById('cardForm');
+const delete_card_at_end_of_day_toggle = card_form.querySelector('input[name="deleteAtEnd"]');
 
 
 new_card_button.addEventListener('click', () => {
@@ -20,7 +22,7 @@ function close_new_card_modal() {
 }
 
 
-document.getElementById('cardForm').addEventListener('submit', function(e) {
+card_form.addEventListener('submit', function(e) {
   e.preventDefault();
   const data = Object.fromEntries(new FormData(this));
   let title = data.title;
@@ -28,7 +30,16 @@ document.getElementById('cardForm').addEventListener('submit', function(e) {
 
   console.log(`Card added:\n${title} | \n${description}`);
   // addCardToList(title, description);
-  socket.emit('new-card', title, description, me);
+  socket.emit('new-card', title, description, me, delete_card_at_end_of_day_toggle.checked);
   close_new_card_modal();
   this.reset();
+});
+
+
+delete_card_at_end_of_day_toggle.addEventListener('click', () => {
+  if (delete_card_at_end_of_day_toggle.checked) {
+    card_form.querySelector('.label').innerText = "Delete at end of day (server will delete it)";
+  } else {
+    card_form.querySelector('.label').innerText = "Stay until self delete (only you can delete it)";
+  }
 });
